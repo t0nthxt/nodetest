ab42ed1f0dbbca8c0f0741a2f9e0c6095e043c67
t0nthxt <piphat1155@gmail.com>
2024-10-25 14:02:39 +0700

    Add all files to project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const debug = require('debug');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get('/',(req,res)=>{
+    res.send('hello NMigga!');
+})
+
+app.listen(port,()=>{
+    console.log("Listen on port",port);
+})
